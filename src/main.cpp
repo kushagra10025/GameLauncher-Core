5175ce8c5be6c1abd638e5e8c5a1fac25f3f125e
Kushagra <kushagra.mckv@gmail.com>
2022-09-08 11:23:51 +0530

    - Added argparse
    - Added nlohmann json
    - Restructure main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <filesystem>
+#include <argparse/argparse.hpp>
+#include <nlohmann/json.hpp>
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "Added Libraries : " << std::endl;
+    std::cout << "\t1. Argparse\n\t2. Nlohmann JSON\n\t3. std::filesystem" << std::endl;
+
+    return 0;
+}
